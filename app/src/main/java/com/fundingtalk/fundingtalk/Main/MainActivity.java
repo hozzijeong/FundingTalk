23822ea74d2a05b7d0faae1111d0df973801da23
Hozzi <ghwls0905@naver.com>
2020-08-22 13:54:35 +0900

    메인 화면 수정

--- app/src/main/java/com/fundingtalk/fundingtalk/Main/MainActivity.java
+++ app/src/main/java/com/fundingtalk/fundingtalk/Main/MainActivity.java
@@ -2,12 +2,14 @@ package com.fundingtalk.fundingtalk.Main;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.fundingtalk.fundingtalk.AppHelper.BaseActivity;
 import com.fundingtalk.fundingtalk.Main.Custom.Fragment.Custom_Main_Fragment;
-import com.fundingtalk.fundingtalk.Main.FAQ.Fragment.Faq_Main_Fragment;
+//import com.fundingtalk.fundingtalk.Main.FAQ.Fragment.Faq_Main_Fragment;
 import com.fundingtalk.fundingtalk.Main.Investment.Fragment.Invest_Main_Fragment;
 import com.fundingtalk.fundingtalk.Main.Loan.Fragment.Loan_Main_Fragment;
 import com.fundingtalk.fundingtalk.R;
@@ -20,9 +22,10 @@ public class MainActivity extends BaseActivity implements BottomNavigationView.O
     public Custom_Main_Fragment custom_main_fragment;
     public Invest_Main_Fragment invest_main_fragment;
     public Loan_Main_Fragment loan_main_fragment;
-    public Faq_Main_Fragment faq_main_fragment;
+//    public Faq_Main_Fragment faq_main_fragment;
     @BindView(R.id.main_bottom_navi) BottomNavigationView bottom_navi;
-
+    public @BindView(R.id.main_layout) ConstraintLayout mainLayout;
+    public @BindView(R.id.sub_layout) ConstraintLayout subLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +44,7 @@ public class MainActivity extends BaseActivity implements BottomNavigationView.O
         custom_main_fragment = new Custom_Main_Fragment();
         invest_main_fragment = new Invest_Main_Fragment();
         loan_main_fragment = new Loan_Main_Fragment();
-        faq_main_fragment = new Faq_Main_Fragment();
+//        faq_main_fragment = new Faq_Main_Fragment();
     }
 
     @Override
@@ -50,19 +53,25 @@ public class MainActivity extends BaseActivity implements BottomNavigationView.O
         switch (menuItem.getItemId()){
             case R.id.main_invest:
                 // 투자하기로 이동(fragment)
+                mainLayout.setVisibility(View.VISIBLE);
+                subLayout.setVisibility(View.INVISIBLE);
                 changeFragment(R.id.main_layout,invest_main_fragment);
                 return true;
             case R.id.main_loan:
                 //대출 받기로 이동
+                mainLayout.setVisibility(View.VISIBLE);
+                subLayout.setVisibility(View.INVISIBLE);
                 changeFragment(R.id.main_layout,loan_main_fragment);
                 return true;
             case R.id.main_custom:
                 // 개인 정보로 이동
-                changeFragment(R.id.main_layout,custom_main_fragment);
+                mainLayout.setVisibility(View.INVISIBLE);
+                subLayout.setVisibility(View.VISIBLE);
+                changeFragment(R.id.sub_layout,custom_main_fragment);
                 return true;
             case R.id.main_faq:
                 // faq로 이동
-                changeFragment(R.id.main_layout,faq_main_fragment);
+//                changeFragment(R.id.main_layout,faq_main_fragment);
                 return true;
 
             default:return false;
